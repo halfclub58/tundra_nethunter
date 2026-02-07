34c51bde852e8c2d153a5f4421a3be1772408d58
Madara273 <ravenhoxs@gmail.com>
2026-02-07 13:24:08 +0000

    fs/include/kernel: import susfs from gki-android12-5.10 branch
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -310,9 +310,10 @@ DEFINE_MUTEX(system_transition_mutex);
  *
  * reboot doesn't sync: do that yourself before calling this.
  */
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) || defined(CONFIG_KSU_SUSFS)
 extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
+
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
@@ -320,8 +321,16 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	char buffer[256];
 	int ret = 0;
 
-#ifdef CONFIG_KSU 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_SUSFS)
 	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+#endif
+#ifdef CONFIG_KSU_SUSFS
+	ret = ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+	if (ret) {
+		goto orig_flow;
+	}
+	return ret;
+orig_flow:
 #endif
 
 	/* We only trust the superuser with rebooting the system. */
